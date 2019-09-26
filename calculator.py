342ca8f9b7f048077a3856347724fdb760fb9642
Ben Lapuhapo <ben.lapuhapo@mths.ca>
2019-09-26 16:49:30 +0000

    Added addition program

---
+++ calculator.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# Created by: Ben Lapuhapo
+# Created on: September 23 2019
+# This program is a simple calculator
+
+
+def main():
+    print("")
+    number_1 = int(input("Please enter the First Number: "))
+    print("")
+    number_2 = int(input("Please enter the Second Number: "))
+    total = number_1 + number_2
+    print("")
+    print('{} + {} = '.format(number_1, number_2), number_1 + number_2)
+
+
+if __name__ == "__main__":
+    main()
